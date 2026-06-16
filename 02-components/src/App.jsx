a122949d7d29a752c314aa37c1be22d893b72e03
baswa143 <prajapnoorbaswaraj@gmail.com>
2026-06-16 09:29:29 +0530

    components

---
+++ 02-components/src/App.jsx
@@ -0,0 +1,17 @@
+import React from 'react'
+import Card from './Components/Card'
+import Navbar from './Components/Navbar'
+
+const App = () => {
+  return (
+    <div>
+      <div className="navbar">
+        thid is navbar
+
+      </div>
+    <Card/>
+    </div>
+  )
+}
+
+export default App
---
+++ 02-components/src/Components/Card.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+
+const Card = () => {
+  return (
+    <div>
+      <div className="card">
+            <h1>Baswaraj Prajapnoor</h1>
+            Lorem ipsum, dolor sit amet consectetur adipisicing elit. Neque, similique!
+          </div>
+    </div>
+  )
+}
+
+export default Card
